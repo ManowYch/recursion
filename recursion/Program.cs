095466bb5ab063897d0b2b691e53e6e3592c9bf1
ManowYch <ychepaykin@gmail.com>
2022-05-04 11:45:04 +0200

    Add project files.

---
+++ recursion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recursion
+{
+    class Program
+    {   static void Foo (ref int[] array, int index) 
+        {
+            Console.WriteLine(array[index]);
+            index++;
+            if (index <= (array.Length-1))
+            {
+                Foo(ref array, index);
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+        }
+
+        static void ElementsSum(ref int[] array, int index, int sum)
+        {
+            if (index <= (array.Length-1))
+            {
+                sum += array[index];
+                index++;
+                ElementsSum(ref array, index, sum);
+            }
+            else
+            {
+                Console.WriteLine(sum);
+                Console.ReadKey();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 4, 3, 1, 5, 3, 2 };
+            Foo(ref myArray, 0);
+            ElementsSum(ref myArray, 0, 0);
+
+        }
+    }
+}
