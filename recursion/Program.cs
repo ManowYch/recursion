91df51e31d2600fabcaec5832fc923a51728b9a0
ManowYch <ychepaykin@gmail.com>
2022-05-04 13:25:40 +0200

    Main created

--- recursion/Program.cs
+++ recursion/Program.cs
@@ -4,56 +4,11 @@ using System.Text;
 
 namespace recursion
 {
+    // Task: Implement array output using recursion.
+    // Find the sum of array elements using recursion.
+    // Find the sum of the digits of a number using recursion. (561 = 12)
     class Program
-    {   static void Foo (ref int[] array, int index) 
-        {
-            Console.WriteLine(array[index]);
-            index++;
-            if (index <= (array.Length-1))
-            {
-                Foo(ref array, index);
-            }
-            else
-            {
-                Console.ReadKey();
-            }
-        }
-
-        static void ElementsSum(ref int[] array, int index, int sum)
-        {
-            if (index <= (array.Length-1))
-            {
-                sum += array[index];
-                index++;
-                ElementsSum(ref array, index, sum);
-            }
-            else
-            {
-                Console.WriteLine(sum);
-                Console.ReadKey();
-            }
-        }
-
-        static void NumbersSum(int userNumber, int result)
-        {
-            int semiresult = 0; 
-            semiresult += userNumber % 10;
-            result += semiresult;
-            userNumber = (userNumber - semiresult) / 10;
-
-            if (userNumber >= 1)
-            {
-                NumbersSum(userNumber, result);
-            }
-            else
-            {
-                userNumber %= 10;
-                result += userNumber;
-                Console.WriteLine(result);
-                Console.ReadKey();
-            }
-
-        }
+    { 
         static void Main(string[] args)
         {
             int[] myArray = { 4, 3, 1, 5, 3, 2 };
